f99bbddc9b1e871fbadbe46e7ff35414f9ec2248
Jordan Marchetto <jordan.marchetto@gmail.com>
2025-03-25 13:41:39 -0400

    initial commit

---
+++ app/controllers/feed_controller.rb
@@ -0,0 +1,80 @@
+class FeedController < ApplicationController
+  def index
+    xml_content = channel_xml + episode_xml + closing_xml
+
+    render xml: xml_content
+  end
+
+  private
+
+  def channel_xml
+    podcast_url = ENV['PODCAST_URL']
+    podcast_title = ENV['PODCAST_TITLE']
+    build_date = Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")
+    copyright = "© #{Time.now.strftime("%Y")} #{ENV['PODCAST_AUTHOR']}"
+    guid = SecureRandom.uuid
+    author = ENV['PODCAST_AUTHOR']
+    description = ENV['PODCAST_DESCRIPTION']
+
+    <<~XML
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:podcast="https://podcastindex.org/namespace/1.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:psc="http://podlove.org/simple-chapters" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+      <atom:link href="#{podcast_url}" rel="self" type="application/rss+xml" />
+      <title>#{podcast_title}</title>
+      <lastBuildDate>#{build_date}</lastBuildDate>
+      <language>en-us</language>
+      <copyright>#{copyright}</copyright>
+      <podcast:locked>yes</podcast:locked>
+      <podcast:guid>#{guid}</podcast:guid>
+      <itunes:author>#{author}</itunes:author>
+      <itunes:type>episodic</itunes:type>
+      <itunes:explicit>false</itunes:explicit>
+      <description><![CDATA[<p>#{description}</p>]]></description>
+      <itunes:owner>
+        <itunes:name>#{author}</itunes:name>
+      </itunes:owner>
+      XML
+  end
+
+  def closing_xml
+    <<~XML
+    </channel>
+    </rss>
+    XML
+  end
+
+  def episode_xml
+    result = ''
+    Dir.glob("#{ENV['PODCAST_EPISODE_PATH']}/*").each do |file|
+      filename = File.basename(file)
+      pubDate = Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")
+      author = ENV['PODCAST_AUTHOR']
+      title = "Ep: #{filename}"
+      description = "Description of #{filename}"
+      length = "18863" # TODO: calculate this
+      duration = "12"
+      type = "audio/mpeg"
+
+      result << <<~XML
+        <item>
+        <itunes:title>#{title}</itunes:title>
+        <title>#{title}</title>
+        <itunes:summary><![CDATA[#{description} ]]></itunes:summary>
+        <description><![CDATA[<p>#{description}</p>]]></description>
+        <content:encoded><![CDATA[<p>#{description}</p>]]></content:encoded>
+        <itunes:author>#{author}</itunes:author>
+        <enclosure url="#{filename}" length="#{length}" type="#{type}" />
+        <guid isPermaLink="false">#{filename}</guid>
+        <pubDate>#{pubDate}</pubDate>
+        <itunes:duration>#{duration}</itunes:duration>
+        <itunes:keywords></itunes:keywords>
+        <itunes:episodeType>full</itunes:episodeType>
+        <itunes:explicit>false</itunes:explicit>
+        </item>
+        XML
+    end
+
+    result
+  end
+end
